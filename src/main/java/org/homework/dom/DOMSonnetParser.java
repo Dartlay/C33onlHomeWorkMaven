93b10be5b824a7e19fc969de7e81dae80634bbdf
Dart lay <ritwinger@gmail.com>
2025-03-17 14:10:34 +0300

    Home Work 23*

--- src/main/java/org/homework/dom/DOMSonnetParser.java
+++ src/main/java/org/homework/dom/DOMSonnetParser.java
@@ -10,36 +10,23 @@ public class DOMSonnetParser {
 
     public void parseXML(String filePath) {
         try {
-            // Получаем InputStream для ресурса
             InputStream inputStream = getClass().getClassLoader().getResourceAsStream(filePath);
-
             if (inputStream == null) {
                 throw new IllegalArgumentException("Файл не найден: " + filePath);
             }
-
-            // Создаем DOM парсер
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
             DocumentBuilder builder = factory.newDocumentBuilder();
             Document document = builder.parse(inputStream);
-
-            // Нормализуем структуру документа
             document.getDocumentElement().normalize();
-
-            // Извлекаем данные
             String firstName = document.getElementsByTagName("firstName").item(0).getTextContent();
             String lastName = document.getElementsByTagName("lastName").item(0).getTextContent();
             String title = document.getElementsByTagName("title").item(0).getTextContent();
-
-            // Собираем содержимое тегов <line>
             StringBuilder linesContent = new StringBuilder();
             NodeList lineNodes = document.getElementsByTagName("line");
             for (int i = 0; i < lineNodes.getLength(); i++) {
                 linesContent.append(lineNodes.item(i).getTextContent()).append("\n");
             }
-
-            // Записываем содержимое в файл
             writeToFile(firstName, lastName, title, linesContent.toString());
-
         } catch (Exception e) {
             e.printStackTrace();
         }
